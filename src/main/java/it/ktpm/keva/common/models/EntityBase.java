fcfb3f0c2c4077044fc8971305918c0bd6f28cba
LeKhoaVan <lekhoavan325802@gmail.com>
2023-05-31 22:02:33 +0700

    setup-code-base

---
+++ src/main/java/it/ktpm/keva/common/models/EntityBase.java
@@ -0,0 +1,84 @@
+package it.ktpm.keva.common.models;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import it.ktpm.keva.common.util.DateTimeUtils;
+import jakarta.persistence.*;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+import org.springframework.data.annotation.*;
+import org.springframework.data.annotation.Version;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class EntityBase implements Serializable {
+    @Id
+    @JdbcTypeCode(SqlTypes.CHAR)
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = innerClass.ID)
+    private UUID id;
+
+    @Version
+    @Column(name = innerClass.VERSION)
+    private int version;
+
+    @CreatedBy
+    @Column(name = innerClass.CREATE_BY)
+    private String createBy;
+
+    @CreatedDate
+    @DateTimeFormat(pattern = DateTimeUtils.DATE_TIME_FORMAT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeUtils.DATE_TIME_FORMAT)
+    @Column(name = innerClass.CREATE_AT)
+    private LocalDateTime createAt;
+
+    @LastModifiedBy
+    @Column(name = innerClass.LAST_MODIFIED_BY)
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @DateTimeFormat(pattern = DateTimeUtils.DATE_TIME_FORMAT)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = DateTimeUtils.DATE_TIME_FORMAT)
+    @Column(name = innerClass.LAST_MODIFIED_AT)
+    private LocalDateTime lastModifiedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        EntityBase that = (EntityBase) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    static class innerClass{
+        static final String ID = "ID";
+        static final String VERSION = "VERSION";
+        static final String CREATE_AT = "CREATE_AT";
+        static final String CREATE_BY = "CREATE_BY";
+        static final String LAST_MODIFIED_AT = "LAST_MODIFIED_AT";
+        static final String LAST_MODIFIED_BY = "LAST_MODIFIED_BY";
+
+    }
+}
+
+
