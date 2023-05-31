a4336d28a6714a1f43377d6211d90abba4a1e2cb
LeKhoaVan <lekhoavan325802@gmail.com>
2023-05-31 12:31:51 +0700

    Initial commit

---
+++ src/main/java/it/ktpm/keva/KevaApplication.java
@@ -0,0 +1,13 @@
+package it.ktpm.keva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KevaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KevaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/ktpm/keva/KevaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.ktpm.keva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
