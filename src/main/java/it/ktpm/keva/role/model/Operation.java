94e2e565f8c4e2600e3382ec2a761f8c6abdc26c
LeKhoaVan <lekhoavan325802@gmail.com>
2023-06-03 21:54:47 +0700

    relationShip role and operation(service)

---
+++ src/main/java/it/ktpm/keva/role/model/Operation.java
@@ -0,0 +1,60 @@
+package it.ktpm.keva.role.model;
+
+import it.ktpm.keva.common.models.EntityBase;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.validator.constraints.Length;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = RoleUtils.Operation.TABLE_NAME)
+public class Operation extends EntityBase {
+    @Column(name = RoleUtils.Operation.CODE, unique = true)
+    @Length(min = 3, max = 10, message = "code in size min = {min} size max = {max}")
+    private String code;
+
+    @Column(name = RoleUtils.Operation.NAME, unique = true)
+    @Length(min = 3, max = 10, message = "name in size min = {min} size max = {max}")
+    private String name;
+
+    @Column(name = RoleUtils.Operation.DESCRIPTION)
+    @NotBlank
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = RoleUtils.Operation.type, nullable = false)
+    private Type type;
+
+    @ManyToMany(mappedBy = RoleUtils.RoleMappedOperation.SERVICE_MAPPED_ROLE)
+    private Set<Role> roles;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Operation operation = (Operation) o;
+        return Objects.equals(this.id, operation.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+
+    public enum Type{
+        SAVE_OR_UPDATE,
+        FETCH,
+        REMOVE
+    }
+}
--- src/main/java/it/ktpm/keva/role/service/impl/RoleServiceImpl.java
+++ src/main/java/it/ktpm/keva/role/service/impl/RoleServiceImpl.java
@@ -1,15 +1,21 @@
 package it.ktpm.keva.role.service.impl;
 
 import it.ktpm.keva.common.util.KevaMapper;
+import it.ktpm.keva.role.dto.OperationDTO;
 import it.ktpm.keva.role.dto.RoleDTO;
+import it.ktpm.keva.role.dto.RoleWithOperationDTO;
+import it.ktpm.keva.role.model.Operation;
 import it.ktpm.keva.role.model.Role;
 import it.ktpm.keva.role.repository.RoleRepository;
+import it.ktpm.keva.role.service.OperationService;
 import it.ktpm.keva.role.service.RoleService;
+import jakarta.validation.ValidationException;
 import org.modelmapper.ModelMapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.UUID;
 
 @Service
@@ -17,10 +23,14 @@ import java.util.UUID;
 public class RoleServiceImpl implements RoleService {
     private final RoleRepository roleRepository;
     private final KevaMapper kevaMapper;
+    private final OperationService operationService;
+    private final KevaMapper mapper;
 
-    public RoleServiceImpl(RoleRepository roleRepository, KevaMapper kevaMapper) {
+    public RoleServiceImpl(RoleRepository roleRepository, KevaMapper kevaMapper, OperationService operationService, KevaMapper mapper) {
         this.roleRepository = roleRepository;
         this.kevaMapper = kevaMapper;
+        this.operationService = operationService;
+        this.mapper = mapper;
     }
 
     @Override
@@ -57,5 +67,18 @@ public class RoleServiceImpl implements RoleService {
         Role roleSave = roleRepository.save(role);
         RoleDTO dto = kevaMapper.map(roleSave,RoleDTO.class);
         return dto;
+    }
+
+    @Override
+    public RoleWithOperationDTO addOperation(UUID roleId,List<UUID> operationIds) {
+        List<Operation> operations = operationService.findAll(operationIds);
+
+        Role role = roleRepository.findById(roleId).orElseThrow(
+                () -> new ValidationException("Role id not exist"));
+
+        operations.forEach(op -> role.addOperation(op));
+        Role roleSave = roleRepository.save(role);
+
+        return mapper.map(roleSave, RoleWithOperationDTO.class);
     }
 }
